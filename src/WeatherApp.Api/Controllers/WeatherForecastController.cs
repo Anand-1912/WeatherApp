8436934f890516c11c62e940df155c9dbc4c0bca
Anand-1912 <34396688+Anand-1912@users.noreply.github.com>
2024-08-25 21:37:11 +0100

    update controller

--- src/WeatherApp.Api/Controllers/WeatherForecastController.cs
+++ src/WeatherApp.Api/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace WeatherApp.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
